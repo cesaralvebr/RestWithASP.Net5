38a704b9280cb3f9e41ab20838371507daa67a1a
Cesar Alves Pereira <cesaralvesitpac@hotmail.com>
2021-06-12 21:16:06 -0300

    business implementation

---
+++ 01_RestWithASP.Net5/RestWithASP.Net5/RestWithASP.Net5/Business/Interfaces/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASP.Net5.Model;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASP.Net5.Business.Interfaces
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long Id);
+        Person Update(Person person);
+        IEnumerable<Person> FindAll();
+        void Delete(long Id);
+    }
+}
