38b9735e8bdb482a1f421238997bc293ba6a2880
Cesar Alves Pereira <cesaralvesitpac@hotmail.com>
2021-06-16 22:39:20 -0300

    Adding the entity books, businesses and repository

---
+++ 01_RestWithASP.Net5/RestWithASP.Net5/RestWithASP.Net5/Business/Implementations/BooksBusinessImplementation.cs
@@ -0,0 +1,41 @@
+﻿using RestWithASP.Net5.Business.InterfacesBusiness;
+using RestWithASP.Net5.Model;
+using RestWithASP.Net5.Repository.Interfaces;
+using System.Collections.Generic;
+
+namespace RestWithASP.Net5.Business.Implementations
+{
+    public class BooksBusinessImplementation : IBooksBusiness
+    {
+        private readonly IBooksRepository _repository;
+
+        public BooksBusinessImplementation(IBooksRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Books> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Books FindById(long Id)
+        {
+            return _repository.FindById(Id);
+        }
+        public Books Create(Books books)
+        {
+            return _repository.Create(books);
+        }
+        public Books Update(Books books)
+        {
+            return _repository.Update(books);
+        }
+        public void Delete(long Id)
+        {
+            _repository.Delete(Id);
+        }
+
+
+    }
+}
---
+++ 01_RestWithASP.Net5/RestWithASP.Net5/RestWithASP.Net5/Business/InterfacesBusiness/IBooksBusiness.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASP.Net5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.Net5.Business.InterfacesBusiness
+{
+   public interface IBooksBusiness
+    {
+        Books Create(Books books);
+        Books FindById(long Id);
+        Books Update(Books books);
+        IEnumerable<Books> FindAll();
+        void Delete(long Id);
+    }
+}
