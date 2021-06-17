a613fde209b5338fe8d36c8c975237a2278b0636
Cesar Alves Pereira <cesaralvesitpac@hotmail.com>
2021-06-16 22:41:00 -0300

    Add Books Controller

---
+++ 01_RestWithASP.Net5/RestWithASP.Net5/RestWithASP.Net5/Controllers/BooksController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASP.Net5.Business.InterfacesBusiness;
+using RestWithASP.Net5.Model;
+using RestWithASP.Net5.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RestWithASP.Net5.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksBusiness _booksBusiness;
+
+        public BooksController(IBooksBusiness booksBusiness)
+        {
+            _booksBusiness = booksBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_booksBusiness.FindAll());
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult Get(long Id)
+        {
+            var books = _booksBusiness.FindById(Id);
+            if (books == null)
+                return NotFound();
+
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Books books)
+        {
+            if (books == null)
+                return BadRequest();
+
+            return Ok(_booksBusiness.Create(books));
+        }
+
+
+        [HttpPut()]
+        public IActionResult Put([FromBody] Books books)
+        {
+            if (books == null)
+                return BadRequest();
+
+            return Ok(_booksBusiness.Update(books));
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(long Id)
+        {
+            _booksBusiness.Delete(Id);
+            return NoContent();
+
+        }
+    }
+}
