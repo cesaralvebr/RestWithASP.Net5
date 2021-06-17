213e9226d9d12ab050f20c1220d467ebcbc841f8
Cesar Alves Pereira <cesaralvesitpac@hotmail.com>
2021-06-16 22:37:39 -0300

    Add Migrations

---
+++ 01_RestWithASP.Net5/RestWithASP.Net5/RestWithASP.Net5/DB/Dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Tocantins', 'Cesar', 'Male', 'CHEFE'),
+	(2, 'Minas Gerais', 'João', 'Male', 'Alves'),
+	(3, 'Rio de Janeiro', 'Natalia', 'Famale', 'Costa'),
+	(4, 'Piauí', 'Maria', 'Famale', 'Paula');
