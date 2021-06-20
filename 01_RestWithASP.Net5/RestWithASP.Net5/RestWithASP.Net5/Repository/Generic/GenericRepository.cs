c780bcebf8b2992f24639958c28a9e208a3de898
Cesar Alves Pereira <cesaralvesitpac@hotmail.com>
2021-06-20 16:45:01 -0300

    Generic Repository

---
+++ 01_RestWithASP.Net5/RestWithASP.Net5/RestWithASP.Net5/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASP.Net5.Context;
+using RestWithASP.Net5.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.Net5.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly MySQLContext _context;
+
+        private DbSet<T> dataset;
+
+        public GenericRepository(MySQLContext mySQLContext)
+        {
+            _context = mySQLContext;
+            dataset = _context.Set<T>();
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return dataset.ToList();
+        }
+
+        public T FindById(long Id)
+        {
+            return dataset.SingleOrDefault(t => t.Id.Equals(Id));
+        }
+
+
+        public T Create(T item)
+        {
+            try
+            {
+                dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+       
+        public T Update(T item)
+        {
+            if (!Exists(item.Id)) return null;
+
+            var _result = dataset.SingleOrDefault(p => p.Id.Equals(item.Id));
+
+            if (_result != null)
+            {
+                try
+                {
+                    dataset.Update(_result);
+                    _context.SaveChanges();
+                    return _result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    
+        public void Delete(long Id)
+        {
+
+            var _result = dataset.SingleOrDefault(p => p.Id.Equals(Id));
+
+
+            if (_result != null)
+            {
+                try
+                {
+                    dataset.Remove(_result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        private bool Exists(long id)
+        {
+            return dataset.Any(p => p.Id.Equals(id));
+        }
+    }
+}
---
+++ 01_RestWithASP.Net5/RestWithASP.Net5/RestWithASP.Net5/Repository/Generic/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASP.Net5.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.Net5.Repository.Generic
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T person);
+        T FindById(long Id);
+        T Update(T person);
+        IEnumerable<T> FindAll();
+        void Delete(long Id);
+    }
+}
